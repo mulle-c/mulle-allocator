10d3d5a3a6bd0539eb33a9cca4bd4f6acc0f6a09
Nat! <nat@mulle-kybernetik.com>
2017-06-17 18:55:27 +0200

    fix formula-info.sh

--- bin/formula-info.sh
+++ bin/formula-info.sh
@@ -1,4 +1,4 @@
-PROJECT="MulleAllocator"   # your project name, requires camel-case
+PROJECT="mulle-allocator"   # your project name, requires camel-case
 DESC="Flexible C memory allocation scheme with leak checking"
 DEPENDENCIES='${DEPENDENCY_TAP}mulle-c11
 ${DEPENDENCY_TAP}mulle-thread'
