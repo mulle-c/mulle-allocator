18478cb117d01b4f080c0ff60ba2d7296fa8dd0e
Nat! <nat@mulle-kybernetik.com>
2020-02-01 15:38:14 +0100

    upgraded mulle-sde

--- src/_mulle-allocator-include.h
+++ src/_mulle-allocator-include.h
@@ -13,12 +13,11 @@
 #define _mulle_allocator_include_h__
 
 // How to tweak the following mulle-c11 #include
-//    remove:        `mulle-sourcetree mark mulle-c11 no-header`
-//    rename:        `mulle-sourcetree mark mulle-c11 set include whatever.h`
-//    make private:  `mulle-sourcetree mark mulle-c11 no-public`
-//    make public:   `mulle-sourcetree mark mulle-c11 public`
-//    make optional: `mulle-sourcetree mark mulle-c11 no-require`
-//    remove for os: `mulle-sourcetree mark mulle-c11 no-os-<osname>`
+//    remove:          `mulle-sourcetree mark mulle-c11 no-header`
+//    rename:          `mulle-sourcetree mark mulle-c11 set include whatever.h`
+//    toggle public:   `mulle-sourcetree mark mulle-c11 [no-]public`
+//    toggle optional: `mulle-sourcetree mark mulle-c11 [no-]require`
+//    remove for os:   `mulle-sourcetree mark mulle-c11 no-os-<osname>`
 # include <mulle-c11/mulle-c11.h>   // mulle-c11
 
 #endif
