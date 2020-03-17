1896361ca2c0fbdf8c8354a71f46d886ed028c9b
Nat! <nat@mulle-kybernetik.com>
2020-03-17 23:54:16 +0100

    add missing environment variables

--- test/.mulle/etc/env/environment-host-travis-ci-prerelease.sh
+++ test/.mulle/etc/env/environment-host-travis-ci-prerelease.sh
@@ -16,3 +16,21 @@ export MULLE_C11_NODETYPE="git"
 export MULLE_C11_URL="https://github.com/mulle-c/mulle-c11.git"
 
 
+#
+# mulle-allocator nodetype: ${MULLE_ALLOCATOR_NODETYPE:-tar}
+#
+export MULLE_ALLOCATOR_NODETYPE="git"
+
+
+#
+# mulle-allocator url: ${MULLE_ALLOCATOR_URL:-https://github.com/mulle-c/mulle-allocator/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_ALLOCATOR_URL="https://github.com/mulle-c/mulle-allocator.git"
+
+
+#
+# mulle-allocator branch: ${MULLE_ALLOCATOR_BRANCH}
+#
+export MULLE_ALLOCATOR_BRANCH="prerelease"
+
+
