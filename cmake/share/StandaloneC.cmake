07fab46208879a01f12b5fd5d01d62d748d67f65
Nat! <nat@codeon.de>
2020-03-26 13:59:14 +0100

    upgrade mulle-sde

--- cmake/share/StandaloneC.cmake
+++ cmake/share/StandaloneC.cmake
@@ -27,7 +27,7 @@ if( STANDALONE)
    endif()
 
    if( NOT STANDALONE_DEFINITIONS)
-      set( STANDALONE_DEFINITIONS ${MULLE_ALLOCATOR_DEFINITIONS})
+      set( STANDALONE_DEFINITIONS ${mulle_allocator_DEFINITIONS})
    endif()
 
    #
