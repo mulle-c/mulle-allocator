a1e83a0621a065b58e8db7754f51788887171cf0
Nat! <nat@mulle-kybernetik.com>
2020-03-26 20:47:50 +0100

    add missing environment file

---
+++ test/.mulle/etc/env/environment-host-travis-ci-prerelease.sh
@@ -0,0 +1,37 @@
+# Fill it with mulle-env envirionment --scope  set <key> <value>
+#
+# mulle-allocator nodetype: ${MULLE_ALLOCATOR_NODETYPE:-tar}
+#
+export MULLE_ALLOCATOR_NODETYPE="git"
+
+
+#
+# mulle-allocator url: ${MULLE_ALLOCATOR_URL:-https://github.com/mulle-c/mulle-allocator/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_ALLOCATOR_URL="https://github.com/mulle-c/mulle-allocator.git"
+
+
+#
+# mulle-allocator branch: ${MULLE_ALLOCATOR_BRANCH}
+#
+export MULLE_ALLOCATOR_BRANCH="prerelease"
+
+
+#
+# mulle-c11 nodetype: ${MULLE_C11_NODETYPE:-tar}
+#
+export MULLE_C11_NODETYPE="git"
+
+
+#
+# mulle-c11 url: ${MULLE_C11_URL:-https://github.com/mulle-c/mulle-c11/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_C11_URL="https://github.com/mulle-c/mulle-c11.git"
+
+
+#
+# mulle-c11 branch: ${MULLE_C11_BRANCH}
+#
+export MULLE_C11_BRANCH="prerelease"
+
+
