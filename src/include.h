mulle-c/mulle-buffer

MULLE_C_EXTERN_GLOBAL for windows dll

src/include.h
@@ -21,6 +21,10 @@
 
 #include "_mulle-buffer-include.h"
 
+
+#ifndef MULLE_BUFFER_EXTERN_GLOBAL
+# define MULLE_BUFFER_EXTERN_GLOBAL MULLE_C_EXTERN_GLOBAL
+#endif
 /* You can add some more include statements here */
 
 #endif

mulle-c/mulle-allocator

src/include.h
@@ -21,6 +21,10 @@
 
 #include "_mulle-allocator-include.h"
 
+#ifndef MULLE_ALLOCATOR_EXTERN_GLOBAL
+# define MULLE_ALLOCATOR_EXTERN_GLOBAL MULLE_C_EXTERN_GLOBAL
+#endif
+
 /* You can add some more include statements here */
 
 #endif